c97ffc31198f0f073755452d0906edaf9d802207
dany han <dany@mdlabs.com>
2018-06-08 00:40:57 -0500

    Remove query-string dependency

--- src/components/AdminPage/index.js
+++ src/components/AdminPage/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 
 import { withRouter, Redirect } from "react-router-dom";
-import qs from "query-string";
+
 import LoginForm from "./LoginForm";
+
+const qs = {
+  parse: str => {
+    const keysAndValues = str.substring(1).split("=");
+
+    return keysAndValues.reduce((acc, item, index) => {
+      if (index % 2 == 0) {
+        acc[item] = decodeURIComponent(keysAndValues[index + 1]);
+      }
+      return acc;
+    }, {});
+  }
+};
 
 export const setUser = user => {
   localStorage.setItem("loggedUser", JSON.stringify(user));
@@ -40,7 +53,6 @@ class AdminPage extends React.Component {
 
     if (location.search) {
       const query = qs.parse(location.search);
-
       if (query.user) {
         try {
           user = JSON.parse(query.user);
