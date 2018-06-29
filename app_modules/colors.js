53d80facebe1f197c5e471dc661bdeaed152562c
dany han <dany@mdlabs.com>
2018-06-28 23:42:13 -0500

    wireframe - css - adding primary and secondary colors + implementing choose city, language, character, and steps component

--- app_modules/colors.js
+++ app_modules/colors.js
@@ -7,3 +7,15 @@ export const primaryTextColor = "#212121";
 export const secondaryTextColor = "#757575";
 export const dividerColor = "#bdbdbd";
 
+// new colors
+
+// new colors
+export const yellowPrimary = "#ffde03";
+export const yellowLight = "#ffff54";
+export const yellowDark = "#c7ad00";
+export const bluePrimary = "#0336ff";
+export const blueLight = "#6463ff";
+export const blueDark = "#0008ca";
+export const pinkPrimary = "#ff0266";
+export const pinkLight = "#ff5c93";
+export const pinkDark = "#c5003c";
--- src/components/StatesPopup/index.js
+++ src/components/StatesPopup/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Label from "@app/Label";
 import join from "@app/join";
+import MobileStepper from "@app/MobileStepper";
 import STATES, { names } from "src/states";
 
 import styles from "./index.scss";
@@ -15,15 +16,22 @@ const IMAGES = {
 
 const StatesPopup = ({ onChange }) => {
   return (
-    <div className={`${styles.statesPopup}`}>
+    <div
+      className={`${
+        styles.statesPopup
+      } flex-ns flex-column pa5 pa0-ns items-center justify-center`}
+    >
       <div
-        className="flex items-center flex-column pa3 pa0-ns items-center justify-center"
-        style={{ minHeight: "100%" }}
+        className="f2 b mt3 mb5 pv3 ph5  bg-yellow-primary"
+        style={{ borderRadius: 24 }}
       >
+        Please choose your state
+      </div>
+      <div className="flex flex-row justify-center">
         {STATES.map((state, index) => (
           <div
             key={state}
-            className={join(styles.stateItem, " fl-ns w-50-ns pa3")}
+            className={join(styles.stateItem, " fl-ns pa3")}
             onClick={() => {
               onChange(state.toLowerCase());
             }}
@@ -45,6 +53,9 @@ const StatesPopup = ({ onChange }) => {
             </div>
           </div>
         ))}
+      </div>
+      <div className="flex justify-center mt3">
+        <MobileStepper style={{ width: 150 }} steps={3} activeStep={1} />
       </div>
     </div>
   );
